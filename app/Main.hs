a84fd39c6673ccad2b08b068f15b39f755a5306a
Pedro Vinicius Coelho belem <pevine6508@gmail.com>
2023-09-26 03:01:04 -0300

    versao funcional 2

--- app/Main.hs
+++ app/Main.hs
@@ -12,10 +12,13 @@ coef f a b = (f b - f a)/(b - a)
 derv f x = coef f x (x + eps) 
 
 linearEq f a b x = coef f a b *(x - a) + f a  
+
+inverLinearEq f a b y = (y - f a)/coef f a b + a
+
 sq x = x*x
 eq a b x = sq (x - a) - b
 func = eq 2 4 
-s = linearEq func 2 4
+-- s = linearEq func 2 4
 
 linearRoot :: Fractional a => (a -> a) -> a -> a -> a
 linearRoot f a b = do
@@ -24,29 +27,30 @@ linearRoot f a b = do
 
 -- { f(b) > 0 > f(a); b > a; f is continuous in (a, b) ; eps > 0 }
 
-anyFuncRoot :: (Double -> Double) -> Double -> Double -> IO ()
-anyFuncRoot f a b = do 
-  -- let fs x = (f x) * ((linearEq f a b) x)
-  let zx = linearRoot f a b
-  let zy = f zx
-  -- let sc = coef f a b
-  -- let p = (1 + f a)/(2*sc)
-  -- let q = (- f a)/(2*sc)
+
+anyFuncRoot f g a b = do 
+  let inverS = inverLinearEq g a b
+  let zx = inverS 0
+  let zy = g zx
+  let sc = abs (coef f a b)
+  let fs x = f x * sc
+  let a1 = (b + zx)/2
+  let b1 = (zx + a)/2
   print (zx, zy, a, b)
 
   if (eps > zy && zy > -eps) || isNaN zy then 
     print (Just zx)
-  else if zy > 0 then
-    anyFuncRoot f zx b
+  else if zy < 0 then
+    anyFuncRoot f fs zx b
   else 
-    anyFuncRoot f a zx
+    anyFuncRoot f fs a zx
 
 main = do
   putStrLn "\tLinearEq between (a, f(a)) & (b, f(b))"
   putStrLn "Number of attempts:"
   print (linearRoot func 3 5)
-  let test x = sq (sq x - x) - x  
-  anyFuncRoot test 5 3
+  let test x = sq (sq x - x) + 2*sq (x-2) - x
+  anyFuncRoot test test 1.4 2 
   -- times <- getLine
   -- let 
   --   loop 0 = return ()
