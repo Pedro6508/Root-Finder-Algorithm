e4e05f5a8ecf1f9b814d84e799861985bcb41483
Pedro Vinicius Coelho belem <pevine6508@gmail.com>
2023-09-26 04:30:18 -0300

    versao 1.0

--- app/Main.hs
+++ app/Main.hs
@@ -6,7 +6,6 @@ import Text.ParserCombinators.ReadP (string)
 import Text.Printf (printf, formatString)
 import Data.Foldable (Foldable(toList))
 
-eps :: Double
 eps = 0.01
 coef f a b = (f b - f a)/(b - a)
 derv f x = coef f x (x + eps) 
@@ -16,51 +15,42 @@ linearEq f a b x = coef f a b *(x - a) + f a
 inverLinearEq f a b y = (y - f a)/coef f a b + a
 
 sq x = x*x
-eq a b x = sq (x - a) - b
-func = eq 2 4 
--- s = linearEq func 2 4
+eq2ndDeg a b x = sq (x - a) - b
+-- s = linearEq eq2nddeg 2 4
 
 linearRoot :: Fractional a => (a -> a) -> a -> a -> a
 linearRoot f a b = do
   let h = linearEq f a b
   (- h a)/coef h a b + a
 
--- { f(b) > 0 > f(a); b > a; f is continuous in (a, b) ; eps > 0 }
+mid f a b = do
+  let tg = coef f a b
+  let s x = tg*(x - a) + f a
+  let g x = tg * f x 
+  let mdot = (b + a)/2
 
-
-anyFuncRoot f g a b = do 
-  let inverS = inverLinearEq g a b
-  let zx = inverS 0
-  let zy = g zx
-  let sc = abs (coef f a b)
-  let fs x = f x * sc
-  let a1 = (b + zx)/2
-  let b1 = (zx + a)/2
-  print (zx, zy, a, b)
-
-  if (eps > zy && zy > -eps) || isNaN zy then 
-    print (Just zx)
-  else if zy < 0 then
-    anyFuncRoot f fs zx b
+  if g mdot > 0 then
+    (g, a, mdot)
   else 
-    anyFuncRoot f fs a zx
+    (g, mdot, b)
+
+findRoot f a b = rfindRoot f (min a b) (max a b)
+
+-- { f(b) > 0 > f(a); b > a; f is continuous in (a, b) ; eps > 0 }
+rfindRoot f a b = do 
+  let (g, x, y) = mid f a b
+  let r = (y + x)/2
+  let diff = abs (y-x)
+
+  print (x, y, r)
+  if (diff < eps) || isNaN r then 
+    print (Just r)
+  else 
+    rfindRoot g x y
 
 main = do
-  putStrLn "\tLinearEq between (a, f(a)) & (b, f(b))"
-  putStrLn "Number of attempts:"
-  print (linearRoot func 3 5)
-  let test x = sq (sq x - x) + 2*sq (x-2) - x
-  anyFuncRoot test test 1.4 2 
-  -- times <- getLine
-  -- let 
-  --   loop 0 = return ()
-  --   loop i = do
-  --     raw_x <- getLine
-  --     let x = (read raw_x :: Double)
-  --     putStr ("f'(" ++ raw_x ++ ") = ")
-  --     let result = s x
-  --     print result
-  --     loop(i-1)
-  -- loop(read times :: Integer)
-  
+  putStrLn "\tRoot Finder Algorithm"
+  let 
+  findRoot eq2ndDeg 1.4 6 
+
   
